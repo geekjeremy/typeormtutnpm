742129c8c78fb477b358b115052d0a0901a13871
Jeremy Simon <geek.jeremy.simon@gmail.com>
2022-11-21 18:21:42 -0800

    purge param in tailwind config deprecated. using content instead

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  content: [],
   theme: {
     extend: {},
   },
